897b6edfe4fbe98e9821ef1e23134d0468b7a276
LACKY5533 <atrashkov.nikita@mail.ru>
2022-05-16 16:44:18 +0300

    GoogleMapGB_Project

---
+++ GoogleMapGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleMapGB
+//
+//  Created by LACKY on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
